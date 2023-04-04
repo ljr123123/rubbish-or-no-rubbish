be072866e930b72b159bc2768ec569cd04395257
Jerry <1422768678@qq.com>
2023-04-04 20:20:54 +0800

    ...

---
+++ controller.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="controller">
+        <div class="move"></div>
+        <img src="../assets/up.svg" class="move"/>
+        <div class="move"></div>
+        <img src="../assets/left.svg" class="move"/>
+        <div class="move"></div>
+        <img src="../assets/right.svg" class="move"/>
+        <div class="move"></div>
+        <img src="../assets/down.svg" class="move"/>
+        <div class="move"></div>
+    </div>
+</template>
+<script setup>
+</script>
+<style scoped>
+.controller{
+    height:200px;
+    width:200px;
+    margin:20px;
+    display: flex;
+    flex-wrap: wrap;
+}
+.move{
+    height:33%;
+    width:33%;
+}
+
+</style>
